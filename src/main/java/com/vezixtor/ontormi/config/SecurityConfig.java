f181222b73deaec8467d9637308df13a1b41cebd
Exploitation\Vezi <vezi.dev@gmail.com>
2018-01-07 23:47:52 -0200

    Security Configure
    allowing any request for now

---
+++ src/main/java/com/vezixtor/ontormi/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.vezixtor.ontormi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .antMatcher("/api/**")
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+}
